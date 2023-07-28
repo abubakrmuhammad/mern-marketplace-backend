14c81eec3b8ef7e9181bfa66f23143218ce51af1
Abubakr Muhammad <realchampiontheno1@gmail.com>
2023-07-28 21:36:29 +0500

    connection to db

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const app = require('./app');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '.env' });
 
 const port = process.env.PORT || 3000;
 
+const DB = process.env.DATABASE_URI.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('Connected to DB. 🚀');
+  });
+
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}. 🤞`);
 });
